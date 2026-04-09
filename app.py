261b2428e3084fb4d2514367d408c543bc81dbe4
Raul Munoz <luraul2366@gmail.com>
2026-04-09 18:17:12 +0200

    Leer archivo CSV con pandas y mostrar filas y columnas

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template, request
+import pandas as pd
 
-#Crear la aplicacion web
-#El __name__ le dice a Flask donde esta el archivo para que encuentre las carpetas templates y static
 app = Flask(__name__)
 
-#Le dice a Flask "cuando alguien entre a la direccion raiz de la web, ejecuta la funcion de abajo", el / es la página principal, como el index
 @app.route('/')
-#La funcion se ejecuta cuando alguien entra a esa ruta, ahora mismo solo devuelve un texto
+
 def index():
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
     archivo = request.files['archivo']
-    return 'Archivo recibido: '+archivo.filename
+    df =pd.read_csv(archivo)
+    filas = len(df)
+    columnas = list(df.columns)
+    return f'Filas: {filas} | Columnas: {columnas}'
 
 if __name__=='__main__':
     app.run(debug=True)
